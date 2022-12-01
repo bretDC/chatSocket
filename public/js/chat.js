c9670bc55ea2815990dd2d31e70f33fb85ca4b26
ZeroCool <bretbickham@gmail.comgit config --global user.password Jthomas1234git config --global user.email bretbickham@gmail>
2022-12-01 09:23:46 -0700

    Chat.js
    added elements to message form, input, button, location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,11 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+
 socket.on('message', (message) => {
     console.log(message)
 })
@@ -7,9 +13,21 @@ socket.on('message', (message) => {
 document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
 
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered')
+    })
 })
 
 document.querySelector('#send-location').addEventListener('click', () => {
@@ -17,5 +35,15 @@ document.querySelector('#send-location').addEventListener('click', () => {
         return alert('Geolocation not supported by your browser')
     }
 
-    navigator.geolocation.getCurrentPosition()
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            $sendLocationButton.removeAttribute('disabled')
+            console.log('Location shared')
+        })
+    })
 })
