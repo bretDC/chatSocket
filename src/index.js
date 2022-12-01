557f40242b5e135bba454152060355607f31314d
ZeroCool <bretbickham@gmail.comgit config --global user.password Jthomas1234git config --global user.email bretbickham@gmail>
2022-12-01 08:49:59 -0700

    initial commit
    added express, nodemon, socket.io
    init using npm
    added src public directories with index.html index.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', () => {
+    console.log('New WebSocket connection')
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        io.emit('countUpdated', count)
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
+})
