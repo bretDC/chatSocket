5cde2ced8ecc825760de6198a35643ec87e98de3
ZeroCool <bretbickham@gmail.comgit config --global user.password Jthomas1234git config --global user.email bretbickham@gmail>
2022-12-01 09:18:12 -0700

    added dependencies
    added bad-words dependency to filter profanity
    
    created bad-words import statement in index.js
    created filter variable in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -18,8 +19,15 @@ io.on('connection', (socket) => {
     socket.emit('message', 'Welcome')
     socket.broadcast.emit('message', 'A new user has joined')
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity not permitted')
+        }
+
         io.emit('message', message)
+        callback()
     })
 
     socket.on('sendLocation', (coords) => {
