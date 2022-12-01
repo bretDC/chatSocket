2d6143a0c5ba6992aca0f0c7325e9a722250d6ee
ZeroCool <bretbickham@gmail.comgit config --global user.password Jthomas1234git config --global user.email bretbickham@gmail>
2022-12-01 09:12:14 -0700

    added location click listener

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,3 @@
-const e = require("express")
-
 const socket = io()
 
 socket.on('message', (message) => {
@@ -12,4 +10,12 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     const message = e.target.elements.message.value
 
     socket.emit('sendMessage', message)
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition()
 })
